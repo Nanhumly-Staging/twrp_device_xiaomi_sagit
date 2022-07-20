42ef46f26f0ae9cbd96a5fc6758287360ddbb5dd
Nanhumly <1607616422@qq.com>
2022-07-20 03:45:39 +0000

    Sorting order

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,8 +25,6 @@
 
 DEVICE_PATH := device/xiaomi/sagit
 
-BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
-
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
@@ -45,6 +43,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a73
 
 # Build
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
