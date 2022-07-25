b74b0b4979cb4b235b521c830ee27b571380233c
nebrassy <nebras30@gmail.com>
2022-07-25 14:51:58 +0000

    sagit: render at 60 FPS Change-Id: I602ec6a5ca4ffbcc01f6eb7e4f35899df0616ae5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,6 +133,7 @@ TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 TW_THEME := portrait_hdpi
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone5/temp
+TW_FRAMERATE := 60
 
 # SEPolicy
 -include device/xiaomi/sagit/sepolicy/recovery-sepolicy.mk
