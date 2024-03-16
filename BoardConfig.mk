5514c08e795a05dafdff39d921b539ce535e9576
Nanhumly <admin@warbler.cc>
2024-03-16 08:13:42 +0000

    sagit: Do not build libxml2
    - libxml2 is built by default
    
    Change-Id: I42a666cd537989c3607923adc2bd3502f1058692

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,13 +102,11 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system//etc/recovery.fstab
 TARGET_RECOVERY_DEVICE_MODULES += \
     android.hardware.vibrator-ndk_platform \
     libion \
-    libxml2 \
     vendor.display.config@1.0 \
     vendor.display.config@2.0
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
 
